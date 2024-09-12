ce5dd27bdc9349a669c43e88dfab72b87a72618d
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-12 10:50:23 +0200

    Initial commit

---
+++ src/main/java/de/ricardo/javarickandmorthyapi/JavaRickAndMorthyApiApplication.java
@@ -0,0 +1,13 @@
+package de.ricardo.javarickandmorthyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRickAndMorthyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRickAndMorthyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ricardo/javarickandmorthyapi/JavaRickAndMorthyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ricardo.javarickandmorthyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRickAndMorthyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
